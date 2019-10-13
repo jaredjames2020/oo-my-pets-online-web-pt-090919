e233eb67d88ef0a51001b2d2ed7cf4375202e71f
Jared S <jaredjames2020@gmail.com>
2019-10-13 23:02:07 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,15 @@
 class Owner
   # code goes here
+  attr_reader :name
+  
+  def initialize(species = "human")
+    @species = species
+  end
+  
+  def say_species
+    @species
+  end
+  
+  def 
+  
 end
