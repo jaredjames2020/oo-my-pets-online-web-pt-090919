e6376e0912856cccdabe462651bba94cc2a477e8
Jared S <jaredjames2020@gmail.com>
2019-10-14 00:25:32 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -52,12 +52,15 @@ class Owner
   end
   
   def sell_pets
-     Dog.all.select {|dog| dog.mood = "nervous"}
-     Cat.all.select {|cat| cat.mood = "nervous"}
-     Dog.all.select {|dog| dog.owner = nil}
-     Cat.all.select {|cat| cat.owner = nil}
+    Dog.all.select {|dog| dog.mood = "nervous"}
+    Cat.all.select {|cat| cat.mood = "nervous"}
+    Dog.all.select {|dog| dog.owner = nil}
+    Cat.all.select {|cat| cat.owner = nil}
   end   
   
+  def list_pets
+    "I have #{@owner.dogs.count} dog(s), and #{@owner.cats.count} cats(s)."
+  end
     
   
 end
